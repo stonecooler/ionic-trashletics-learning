09d7b0f5d04b4b9d3d8ac989249dee70cb17b2a7
Stephan Steinkuehler <st.stonecooler@gmail.com>
2019-10-24 17:01:46 +0200

    Init Commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
+import { PhotoService } from '../services/photo.service';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  currentImage: any;
 
-  constructor() {}
+  constructor(public photoService: PhotoService) {  }
+
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
+
+// import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google: any;
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +11,80 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  @ViewChild('map',{static:false}) mapElement: ElementRef;
+  map: any;
+
+  // constructor(public geolocation: Geolocation) {}
   constructor() {}
+
+  // ionViewDidLoad(){
+  //   this.loadMap();
+  // }
+
+  loadMap(){
+    console.log('loadMap');
+
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    // ----------------
+
+    // this.geolocation.getCurrentPosition().then((position) => {
+
+    //   let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    //   let mapOptions = {
+    //     center: latLng,
+    //     zoom: 15,
+    //     mapTypeId: google.maps.MapTypeId.ROADMAP
+    //   }
+
+    //   this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    // }, (err) => {
+    //   console.log(err);
+    // });
+
+    // ----------------
+
+    // this.geolocation.getCurrentPosition((position)=>{
+    //     let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    //     let mapOptions = {
+    //       center: latLng,
+    //       zoom: 15,
+    //       mapTypeId: google.maps.MapTypeId.ROADMAP
+    //     }
+
+    //     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    // },(err) => {
+    //     console.log(err);
+    // });
+
+
+  }
+
+  ionViewWillEnter() {
+    console.log("ionViewWillEnter");
+  }
+
+  ionViewDidEnter() {
+    console.log("ionViewDidEnter");
+    this.loadMap();
+  }
+
+  ionViewWillLeave() {
+    console.log("ionViewWillLeave");
+  }
+
+  ionViewDidLeave() {
+    console.log("ionViewDidLeave");
+  }
 
 }
